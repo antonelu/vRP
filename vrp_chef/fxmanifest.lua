9db7add2898f7916e9cd90be54b9d67d294e9a1e
warfa <63048792+warfas@users.noreply.github.com>
2022-05-01 15:41:12 +0300

    Add files via upload

---
+++ vrp_chef/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "adamant"
+games {'gta5'}
+author "warfa"
+
+client_scripts{
+  "@vrp/client/Tunnel.lua",
+  "@vrp/client/Proxy.lua",
+  "client.lua"
+}
+
+server_scripts{ 
+  "@vrp/lib/utils.lua",
+  "server.lua"
+}
+
